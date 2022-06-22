347df87c295937711dfcd018949560c4ee75b07f
Mrityunjay Mishra <mrityunjaymishra@hotmail.com>
2022-06-22 06:42:28 +0530

    env_var updated for SMTP

--- helper/mailsendingfunction.js
+++ helper/mailsendingfunction.js
@@ -1,40 +1,48 @@
 const nodemailer = require("nodemailer")
-const verify = process.env.YOUR_DOMAIN || "http://localhost:3000"
+const DOMAIN = process.env.YOUR_DOMAIN || "http://localhost:3000"
 
 module.exports.mailForVerify = async (email, token) => {
   const smtp = nodemailer.createTransport({
-    host: "smtp-relay.sendinblue.com",
-    port: 587,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
     secure: false,
     auth: {
-      user: "achyutkr1122@gmail.com",
-      pass: "bMUf7pP9aOhg8EsC",
+      user: process.env.SMTP_AUTH_USER,
+      pass: process.env.SMTP_AUTH_PASSWORD,
     },
   })
   const hello = await smtp.sendMail({
     to: email,
     from: "achyutkr1122@gmail.com",
     subject: "Mail Verification",
-    html: `Click here to verify:  <br> <a href="${verify}/user/login/${token}">${token}</a>`,
+    html: `Click here to verify:  
+            <br> 
+            <a href="${DOMAIN}/user/login/${token}">
+            ${DOMAIN}/user/login/${token}
+            </a>`,
   })
   return hello
 }
 
 module.exports.mailForForgetpassword = async (email, token) => {
   const smtp = nodemailer.createTransport({
-    host: "smtp-relay.sendinblue.com",
-    port: 587,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
     secure: false,
     auth: {
-      user: "achyutkr1122@gmail.com",
-      pass: "bMUf7pP9aOhg8EsC",
+      user: process.env.SMTP_AUTH_USER,
+      pass: process.env.SMTP_AUTH_PASSWORD,
     },
   })
   const hello = await smtp.sendMail({
     to: email,
     from: "achyutkr1122@gmail.com",
     subject: "Mail Verification",
-    html: `Click here to verify it's you:  <br> <a href="${verify}/user/detailforchange/${token}">${token}</a>`,
+    html: `Click here to verify it's you:  
+              <br> 
+              <a href="${DOMAIN}/user/detailforchange/${token}">
+                ${DOMAIN}/user/detailforchange/${token}
+              </a>`,
   })
   return hello
 }
